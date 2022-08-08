8a6dc44d435ab66389f123dccd1a295ccda5d052
dangelomedinag <dangelomedinag@gmail.com>
2022-08-08 12:23:40 -0400

    xxx

---
+++ src/routes/magacine.svelte
@@ -0,0 +1,1 @@
+<h1>Magacine Page Info</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV !== 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
