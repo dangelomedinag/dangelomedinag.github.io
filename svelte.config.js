56bbacc1b531dbe9e46fece9ee5854aad7af84af
dangelomedinag <dangelomedinag@gmail.com>
2022-08-08 16:21:19 -0400

    build outdir docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV !== 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
 			fallback: '404.html'
 		}),
-		paths: {
-			base: dev ? '' : '/dangelomedinag.github.io'
-		},
 		appDir: 'internal',
 		prerender: {
 			crawl: true,
