963a0870035608b908342aa3374d9e70c09ef8ad
dangelomedinag <dangelomedinag@gmail.com>
2024-03-26 18:09:47 -0300

    add base_path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
+	<a href="{base}/">home</a>
+	<a href="{base}/about">about</a>
 </nav>
 <slot />
