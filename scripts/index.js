6f37df7d01d20b7dcb0459cd30566773bbadd7cb
dangelomedinag <dangelomedinag@gmail.com>
2026-04-07 21:09:04 +0000

    visualizar resultado

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,12 @@ const KEY_COUNT = Symbol("count")
 
 window['ai_edge_gallery_get_result'] = async (dataStr) => {
   const action = dataStr.action
+  const amount = dataStr.amount
+  const date = dataStr.date
 
   try {
     // Points the app directly to your local UI!
-    const fullUrl = `ui.html?v=${Date.now()}`;
+    const fullUrl = `ui.html?v=${Date.now()}&data=${JSON.stringify({amount,date, action})}`;
 
     return JSON.stringify({
       webview: {url: fullUrl},
